2f9f3b5c4b1b4d622fd6fd5a895ea19c320d5abb
phenirain <phenirain@phenirains-MacBook-Pro.local>
2025-01-24 00:59:55 +0300

    feat: added generic repository

---
+++ internal/infrastructure/db/repository.go
@@ -0,0 +1,93 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"reflect"
+	"strings"
+)
+
+type Model interface {
+	SetId(id int32)
+}
+
+type DbModel[T Model] interface {
+	FromModelToDB(model *T)
+	TableName() string
+	ID() int32
+}
+
+type Repository[TDB DbModel[T], T Model] struct {
+	db *sqlx.DB
+}
+
+func NewRepository[TDB DbModel[T], T Model](db *sqlx.DB) *Repository[TDB, T] {
+	return &Repository[TDB, T]{db: db}
+}
+
+func (r *Repository[TDB, T]) Create(ctx context.Context, model *T) (*T, error) {
+	var dbModel TDB
+	dbModel.FromModelToDB(model)
+
+	val := reflect.ValueOf(dbModel)
+	typ := reflect.TypeOf(dbModel)
+	fields := make([]string, 0, typ.NumField()-1)
+	args := make([]interface{}, 0, typ.NumField()-1)
+	argsIds := make([]string, 0, typ.NumField()-1)
+
+	for i := 0; i < typ.NumField(); i++ {
+		if typ.Field(i).Name == "Id" {
+			continue
+		}
+		fields = append(fields, typ.Field(i).Name)
+		argsIds = append(argsIds, fmt.Sprintf("$%d", len(args)+1))
+		args = append(args, val.Field(i))
+	}
+	query := fmt.Sprintf(`INSERT INTO %s (%s) VALUES (%s)`, dbModel.TableName(), strings.Join(fields, ", "+
+		""), strings.Join(argsIds, ", "))
+
+	var id int32
+	err := r.db.QueryRowxContext(ctx, query, args...).Scan(&id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert to %s: %v", dbModel.TableName(), err)
+	}
+	(*model).SetId(id)
+	return model, nil
+}
+
+func (r *Repository[TDB, T]) Update(ctx context.Context, model *T) error {
+	var dbModel TDB
+	dbModel.FromModelToDB(model)
+
+	val := reflect.ValueOf(dbModel)
+	typ := reflect.TypeOf(dbModel)
+	fields := make([]string, 0, typ.NumField()-1)
+	args := make([]interface{}, 0, typ.NumField()-1)
+
+	for i := 0; i < typ.NumField(); i++ {
+		if typ.Field(i).Name == "Id" {
+			continue
+		}
+		fields = append(fields, fmt.Sprintf("%s = $%d", typ.Field(i).Name, len(args)+1))
+		args = append(args, val.Field(i))
+	}
+
+	query := fmt.Sprintf(`UPDATE %s SET %s WHERE id = $%d`, dbModel.TableName(), strings.Join(fields, ", "), dbModel.ID())
+
+	_, err := r.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("failed to update %s with id = %d: %v", dbModel.TableName(), dbModel.ID(), err)
+	}
+	return nil
+}
+
+func (r *Repository[TDB, T]) Delete(ctx context.Context, id int32) error {
+	var dbModel TDB
+	query := fmt.Sprintf(`DELETE FROM %s WHERE id = $1`, dbModel.TableName())
+	_, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("failed to delete %s with id = %d: %v", dbModel.TableName(), id, err)
+	}
+	return nil
+}
