b02b408c2efcf2e3215568697d59b6523720e44f
phenirain <phenirain@phenirains-MacBook-Pro.local>
2025-01-24 22:48:54 +0300

    feat: realize business logic of sell

---
+++ internal/service/orders/sell.go
@@ -0,0 +1,75 @@
+package orders
+
+import (
+	"api/internal/domain/orders"
+	mainService "api/internal/service"
+	"context"
+	"fmt"
+)
+
+func (s *OrderService) ReserveOrder(ctx context.Context, request CreateOrderRequest) (*orders.Order, error) {
+	// Implement reservation logic here
+	// Check if product is available, update stock, and create order
+	product, err := s.productRepo.GetById(ctx, request.ProductId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get product: %v", err)
+	}
+	if product.Quantity < request.Quantity {
+		return nil, ErrInsufficientStock
+	}
+	product.ReservedQuantity += request.Quantity
+	err = s.productRepo.Update(ctx, product)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update product: %v", err)
+	}
+	order, err := request.ToModel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create order: %v", err)
+	}
+	order.Status = orders.OrderStatusReserved
+	order, err = s.Repository.Create(ctx, order)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create order: %v", err)
+	}
+	return order, nil
+}
+
+func (s *OrderService) CompletedOrder(ctx context.Context, request UpdateOrderRequest) error {
+	exists, err := s.Repository.ExistsById(ctx, request.Id)
+	if err != nil {
+		return fmt.Errorf("failed to check order existence: %v", err)
+	}
+	if !exists {
+		return mainService.ErrNotFound("order", request.Id)
+	}
+	exists, err = s.productRepo.ExistsById(ctx, request.ProductId)
+	if err != nil {
+		return fmt.Errorf("failed to check product existence: %v", err)
+	}
+	if !exists {
+		return mainService.ErrNotFound("product", request.ProductId)
+	}
+	product, err := s.productRepo.GetById(ctx, request.ProductId)
+	if err != nil {
+		return fmt.Errorf("failed to get product: %v", err)
+	}
+	if product.Quantity < request.Quantity {
+		return ErrInsufficientStock
+	}
+	product.Quantity -= request.Quantity
+	product.ReservedQuantity -= request.Quantity
+	err = s.productRepo.Update(ctx, product)
+	if err != nil {
+		return fmt.Errorf("failed to update product: %v", err)
+	}
+	order, err := request.ToModel()
+	if err != nil {
+		return fmt.Errorf("failed to update order: %v", err)
+	}
+	order.Status = orders.OrderStatusCompleted
+	err = s.Repository.Update(ctx, order)
+	if err != nil {
+		return fmt.Errorf("failed to update order: %v", err)
+	}
+	return nil
+}
--- internal/service/orders/service.go
+++ internal/service/orders/service.go
@@ -1,15 +1,71 @@
 package orders
 
 import (
+	domClient "api/internal/domain/clients"
 	domOrder "api/internal/domain/orders"
+	domProduct "api/internal/domain/products"
 	"api/internal/infrastructure/orders"
 	"api/internal/service"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/shopspring/decimal"
+	"time"
 )
 
-type OrderService struct {
-	service.Repository[*orders.OrderDB, *domOrder.Order]
+var (
+	ErrInsufficientStock = errors.New("insufficient stock")
+)
+
+type ProductRepository interface {
+	ExistsById(ctx context.Context, id int32) (bool, error)
+	GetById(ctx context.Context, id int32) (*domProduct.Product, error)
+	Update(ctx context.Context, model *domProduct.Product) error
 }
 
-func NewOrderService(repo service.Repository[*orders.OrderDB, *domOrder.Order]) *OrderService {
-	return &OrderService{repo}
+type OrderService struct {
+	*service.Service[*CreateOrderRequest, *UpdateOrderRequest, *domOrder.Order, *orders.OrderDB]
+	productRepo ProductRepository
+}
+
+func NewOrderService(
+	orderRepo service.Repository[*orders.OrderDB, *domOrder.Order],
+	productRepo ProductRepository,
+) *OrderService {
+	orderService := service.NewService[*CreateOrderRequest, *UpdateOrderRequest, *domOrder.Order,
+		*orders.OrderDB](orderRepo)
+	return &OrderService{Service: orderService, productRepo: productRepo}
+}
+
+type CreateOrderRequest struct {
+	ProductId  int32                `json:"product_id"`
+	ClientId   int32                `json:"client_id"`
+	Date       time.Time            `json:"order_date"`
+	Status     domOrder.OrderStatus `json:"order_status"`
+	Quantity   int32                `json:"quantity"`
+	TotalPrice decimal.Decimal      `json:"total_price"`
+}
+
+func (cro *CreateOrderRequest) ToModel() (*domOrder.Order, error) {
+	order, err := domOrder.CreateOrder(domProduct.Product{Id: cro.ProductId},
+		domClient.Client{Id: cro.ClientId},
+		cro.Date, cro.Status, cro.Quantity, cro.TotalPrice)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create order: %v", err)
+	}
+	return order, nil
+}
+
+type UpdateOrderRequest struct {
+	Id int32 `json:"id"`
+	*CreateOrderRequest
+}
+
+func (uor *UpdateOrderRequest) ToModel() (*domOrder.Order, error) {
+	order, err := domOrder.NewOrder(uor.Id, domProduct.Product{Id: uor.ProductId},
+		domClient.Client{Id: uor.ClientId}, uor.Date, uor.Status, uor.Quantity, uor.TotalPrice)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create order: %v", err)
+	}
+	return order, nil
 }
