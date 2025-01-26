d2997f72f1e50413e2a09baeae81e5b7d734d085
phenirain <bigrus1998@gmail.com>
2025-01-26 13:28:37 +0300

    feat: added first step of fill in migration

---
+++ migrations/20250126091416_fillIn.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO roles (role_name)
+VALUES
+('Manager'),
+('StorageManager'),
+('SalesManager'),
+('Logician');
+
+INSERT INTO product_categories(category_name)
+VALUES
+('Семена'),
+('Удобрения'),
+('Техника');
